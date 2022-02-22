e7bb6b98662530aa85b3bd37a7bade52dba2f72c
Ardmona <100177108+Ardmona@users.noreply.github.com>
2022-02-22 15:19:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,19 +26,23 @@ app = dash.Dash()   #initialising dash app
 
 csv = pd.read_csv('https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state_cumulative.csv')
 csv.date = pd.to_datetime(csv.date)
-
+date_wise_df = csv.groupby(['date', 'state'])['positives'].sum().reset_index()
 
 def stock_prices():
     # Function for creating line chart showing Google stock prices over time 
-    fig = go.Figure([go.Scatter(x = csv['date'], y = csv['positives'],\
-                     line = dict(color = 'firebrick', width = 4), name = 'Google')
-                     ])
+    #date_wise_df = csv.groupby(['date', 'state'])['positives'].sum().reset_index()
+    fig = px.bar(date_wise_df, x= 'date', y = 'positives', color = 'state')
+                    
+                
     fig.update_layout(title = 'Prices over time',
                       xaxis_title = 'Dates',
                       yaxis_title = 'Prices'
                       )
     return fig  
 
+ 
+ 
+ 
  
 app.layout = html.Div(id = 'parent', children = [
     html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
@@ -48,3 +52,7 @@ app.layout = html.Div(id = 'parent', children = [
         dcc.Graph(id = 'line_plot', figure = stock_prices())    
     ]
                      )
+
+
+if __name__ == '__main__': 
+    app.run_server()
